df2dba926df28943a8ab90faa1f68f871f71c333
Marsilas Wondimagegnehu <124493099+Marsill@users.noreply.github.com>
2024-12-30 12:30:56 -0600

    Update login_page.dart

--- planningapp/lib/features/user_auth/present/pages/login_page.dart
+++ planningapp/lib/features/user_auth/present/pages/login_page.dart
@@ -1,10 +1,52 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:planningapp/features/user_auth/present/pages/signup_page.dart';
 import 'package:planningapp/features/user_auth/present/widget/contain_form.dart';
 import 'package:planningapp/features/user_auth/present/pages/home_page.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _login() async {
+    String email = _emailController.text;
+    String password = _passwordController.text;
+
+    if (email.isEmpty || password.isEmpty) {
+      print("Please enter both email and password");
+    } else {
+      try {
+        UserCredential userCredential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+
+        if (userCredential.user != null) {
+          print("User logged in successfully");
+          Navigator.pushReplacement(
+            context,
+            MaterialPageRoute(builder: (context) => HomePage()),
+          );
+        }
+      } catch (e) {
+        print("Error occurred during login: $e");
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,63 +55,65 @@ class LoginPage extends StatelessWidget {
         title: Text("Login"),
         backgroundColor: Colors.cyan,
       ),
-      body: Center( 
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Login", style: TextStyle(
-              fontSize: 29, fontWeight: FontWeight.bold),
+            children: [
+              Text(
+                "Login",
+                style: TextStyle(fontSize: 29, fontWeight: FontWeight.bold),
               ),
-              SizedBox(
-                height: 20,
-              ),
+              SizedBox(height: 20),
               FormContainer(
+                controller: _emailController,
                 hintText: "Email",
                 isPasswordField: false,
               ),
-              SizedBox(
-                height: 20,
-              ),
+              SizedBox(height: 20),
               FormContainer(
+                controller: _passwordController,
                 hintText: "Password",
                 isPasswordField: true,
               ),
-              SizedBox(               // the login button structure 
-                height: 20,),
+              SizedBox(height: 20),
               GestureDetector(
-                  onTap: (){
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
-                  },
-                child:Container(
+                onTap: _login,
+                child: Container(
                   width: double.infinity,
                   height: 40,
                   decoration: BoxDecoration(
                     color: Colors.cyan,
-                    borderRadius: BorderRadius.circular(10)
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  child: Center(child: Text("Login" , style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
-                  )
+                  child: Center(
+                    child: Text(
+                      "Login",
+                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ),
                 ),
               ),
-              SizedBox(height: 20,),
+              SizedBox(height: 20),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text("Don't have an account?"),
-                  SizedBox(
-                    width: 5,),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pushAndRemoveUntil(
-                          context,
-                          MaterialPageRoute(builder: (context) => SignupPage()),
-                          (Route<dynamic> route) => false,
-                        );
-                      },
-                      child: Text("Sign Up", style: TextStyle(color: Colors.cyan, fontWeight: FontWeight.bold),),
-                    )
+                  SizedBox(width: 5),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(builder: (context) => SignupPage()),
+                        (route) => false,
+                      );
+                    },
+                    child: Text(
+                      "Sign Up",
+                      style: TextStyle(color: Colors.cyan, fontWeight: FontWeight.bold),
+                    ),
+                  ),
                 ],
               ),
             ],
