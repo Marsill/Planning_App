eefb9b2f7cb9633b93c80fcad1a0489f9ba6b87e
Marsilas Wondimagegnehu <124493099+Marsill@users.noreply.github.com>
2024-12-30 12:44:14 -0600

    Update signup_page.dart

--- planningapp/lib/features/user_auth/present/pages/signup_page.dart
+++ planningapp/lib/features/user_auth/present/pages/signup_page.dart
@@ -1,9 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:planningapp/features/user_auth/firebase_auth/firebase_auth.dart';
 import 'package:planningapp/features/user_auth/present/pages/login_page.dart';
 import 'package:planningapp/features/user_auth/present/widget/contain_form.dart';
 
-class SignupPage extends StatelessWidget {
+class SignupPage extends StatefulWidget {
   const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final FirebaseAuthService _auth = FirebaseAuthService();
+
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,61 +32,66 @@ class SignupPage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Signup"),
       ),
-      body: Center( 
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Signup", style: TextStyle(
-              fontSize: 29, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(
-                height: 20,
-              ),
+            children: [
+              Text("Signup", style: TextStyle(fontSize: 29, fontWeight: FontWeight.bold)),
+              SizedBox(height: 20),
               FormContainer(
+                controller: _usernameController,
                 hintText: "Username",
                 isPasswordField: false,
               ),
-              SizedBox(
-                height: 20,
-              ),
+              SizedBox(height: 20),
               FormContainer(
+                controller: _emailController,
                 hintText: "Email",
                 isPasswordField: false,
               ),
-              SizedBox(
-                height: 20,
-              ),
+              SizedBox(height: 20),
               FormContainer(
+                controller: _passwordController,
                 hintText: "Password",
                 isPasswordField: true,
               ),
-              SizedBox(               
-                height: 20,),
-                Container(
+              SizedBox(height: 20),
+              GestureDetector(
+                onTap: _signUp,
+                child: Container(
                   width: double.infinity,
                   height: 40,
                   decoration: BoxDecoration(
                     color: Colors.orange,
-                    borderRadius: BorderRadius.circular(10)
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  child: Center(child: Text("Signup" , style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold)),
+                  child: Center(
+                    child: Text(
+                      "Signup",
+                      style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ),
                 ),
               ),
-              SizedBox(height: 20,),
+              SizedBox(height: 20),
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   Text("Already have an account?"),
-                  SizedBox(
-                    width: 5,),
-                    GestureDetector(
-                      onTap: () {
-                        Navigator.pushAndRemoveUntil( context, MaterialPageRoute(builder: (context) => LoginPage()), (Route<dynamic> route) => false, );
-                      },
-                      child: Text("Login", style: TextStyle(color: Colors.cyan, fontWeight: FontWeight.bold),),
-                    )
+                  SizedBox(width: 5),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(builder: (context) => LoginPage()),
+                        (route) => false,
+                      );
+                    },
+                    child: Text("Login",style: TextStyle(color: Colors.cyan, fontWeight: FontWeight.bold),
+                    ),
+                  ),
                 ],
               ),
             ],
@@ -74,4 +100,30 @@ class SignupPage extends StatelessWidget {
       ),
     );
   }
+
+  void _signUp() async {
+  String username = _usernameController.text;
+  String email = _emailController.text;
+  String password = _passwordController.text;
+
+  if (username.isEmpty || email.isEmpty || password.isEmpty) {
+    print("Please fill all fields.");
+    return; 
+  }
+
+  try {
+    User? user = await _auth.signUpWithEmailAndPassword(email, password);
+
+    if (user != null) {
+      print("User created successfully");
+
+      Navigator.pushReplacementNamed(context, "/home");  
+    } else {
+      print("Error occurred during sign up");
+    }
+  } catch (e) {
+    print("Error: $e");
+  }
+}
+
 }
