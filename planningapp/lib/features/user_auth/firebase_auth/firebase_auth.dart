ae9d62d330587d1ff649baa422fd970e1b35dc43
Marsilas Wondimagegnehu <124493099+Marsill@users.noreply.github.com>
2024-12-30 20:15:40 -0600

    Update firebase_auth.dart

--- planningapp/lib/features/user_auth/firebase_auth/firebase_auth.dart
+++ planningapp/lib/features/user_auth/firebase_auth/firebase_auth.dart
@@ -1,17 +1,48 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class AuthService {
+class FirebaseAuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  Future<User?> signUpWithEmailAndPassword(String email, String password) async {
+  Future<User?> signUpWithEmailAndPassword(String username, String email, String password) async {
     try {
-      UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
-      return result.user;
+      UserCredential credential = await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      User? user = credential.user;
+
+      if (user != null) {
+        await user.updateDisplayName(username);
+        await user.reload();
+
+        await _firestore.collection("users").doc(user.uid).set({
+          "username": username,
+          "email": email,
+          "phoneNumber": "", 
+          "address": "",     
+        });
+
+        return user;
+      }
     } catch (e) {
-      print(e.toString());
-      return null;
+      print("Error occurred during sign-up: $e");
     }
+    return null;
   }
 
-  // Add more methods as needed
+  Future<User?> signInWithEmailAndPassword(String email, String password) async {
+    try {
+      UserCredential credential = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      return credential.user;
+    } catch (e) {
+      print("Error occurred during log-in: $e");
+    }
+    return null;
+  }
 }
